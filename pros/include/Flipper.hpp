e6f2a6b2cc9ee62e33030ed124609210c821a0dd
erikorlowski <erik.orlowski@gmail.com>
2019-02-12 16:37:28 -0600

    Creating autonomous functions for flipper.

--- pros/include/Flipper.hpp
+++ pros/include/Flipper.hpp
@@ -4,12 +4,18 @@
 class Flipper{
 	pros::Motor* motor1;
 	pros::Motor* motor2;
+	double DOWN_POSITION;
+	double UP_POSITION;
 public:
 	Flipper();
 	void initFlipper(int motor1Port, int motor2Port);
 	void set(int speed);
 	void down();
+	bool isDown();
+	void downBlocking();
 	void up();
+	bool isUp();
+	void upBlocking();
 	void printMotorTemps();
 };
 
--- pros/src/Flipper.cpp
+++ pros/src/Flipper.cpp
@@ -10,6 +10,9 @@ void Flipper::initFlipper(int motor1Port, int motor2Port)
 {
 	motor1 = new Motor((std::uint8_t) abs(motor1Port), E_MOTOR_GEARSET_18, motor1Port < 0, E_MOTOR_ENCODER_DEGREES);
 	motor2 = new Motor((std::uint8_t) abs(motor2Port), E_MOTOR_GEARSET_18, motor2Port < 0, E_MOTOR_ENCODER_DEGREES);
+
+	UP_POSITION = -840;
+	DOWN_POSITION = -380;
 }
 
 void Flipper::set(int speed)
@@ -20,14 +23,40 @@ void Flipper::set(int speed)
 
 void Flipper::down()
 {
-	motor1->move_absolute(-840, 127);
-	motor2->move_absolute(-840, 127);
+	motor1->move_absolute(UP_POSITION, 127);
+	motor2->move_absolute(UP_POSITION, 127);
+}
+
+bool Flipper::isDown()
+{
+	double averagePosition = (motor1->get_position() + motor2->get_position()) / 2.0;
+	return std::abs(averagePosition - DOWN_POSITION) < 2;
+}
+
+void Flipper::downBlocking()
+{
+	do{
+		down();
+	}while(!isDown());
 }
 
 void Flipper::up()
 {
-	motor1->move_absolute(-380, 127);
-	motor2->move_absolute(-380, 127);
+	motor1->move_absolute(DOWN_POSITION, 127);
+	motor2->move_absolute(DOWN_POSITION, 127);
+}
+
+bool Flipper::isUp()
+{
+	double averagePosition = (motor1->get_position() + motor2->get_position()) / 2.0;
+	return std::abs(averagePosition - UP_POSITION) < 2;
+}
+
+void Flipper::upBlocking()
+{
+	do{
+		up();
+	}while(!isUp());
 }
 
 void Flipper::printMotorTemps()
