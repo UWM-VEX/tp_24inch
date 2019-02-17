ca858f6354d14db818999d7ad57845f8d8f8cecd
Jeremy Wissell <43767568+BassBoss4121@users.noreply.github.com>
2019-02-17 16:02:09 -0600

    Update Lift.cpp

--- pros/src/Lift.cpp
+++ pros/src/Lift.cpp
@@ -8,10 +8,10 @@ Lift::Lift() {};
 
 void Lift::initLift(int leftPort, int rightPort, int left2Port, int right2Port)
 {
-	left = new Motor((std::uint8_t) abs(leftPort), E_MOTOR_GEARSET_18, leftPort < 0, E_MOTOR_ENCODER_ROTATIONS);
-	left2 = new Motor((std::uint8_t) abs(left2Port), E_MOTOR_GEARSET_18, left2Port < 0, E_MOTOR_ENCODER_ROTATIONS);
-	right = new Motor((std::uint8_t) abs(rightPort), E_MOTOR_GEARSET_18, rightPort < 0, E_MOTOR_ENCODER_ROTATIONS);
-	right2 = new Motor((std::uint8_t) abs(right2Port), E_MOTOR_GEARSET_18, right2Port < 0, E_MOTOR_ENCODER_ROTATIONS);
+	left = new Motor((std::uint8_t) abs(leftPort), E_MOTOR_GEARSET_18, leftPort < 0, E_MOTOR_ENCODER_DEGREES);
+	left2 = new Motor((std::uint8_t) abs(left2Port), E_MOTOR_GEARSET_18, left2Port < 0, E_MOTOR_ENCODER_DEGREES);
+	right = new Motor((std::uint8_t) abs(rightPort), E_MOTOR_GEARSET_18, rightPort < 0, E_MOTOR_ENCODER_DEGREES);
+	right2 = new Motor((std::uint8_t) abs(right2Port), E_MOTOR_GEARSET_18, right2Port < 0, E_MOTOR_ENCODER_DEGREES);
 }
 
 void Lift::set(int speed)
@@ -24,10 +24,10 @@ void Lift::set(int speed)
 
 void Lift::travelh()
 {
-left->move_absolute(-50, 127);
-left2->move_absolute(-50, 127);
-right->move_absolute(-50, 127);
-right2->move_absolute(-50, 127);
+left->move_absolute(50, 127);
+left2->move_absolute(50, 127);
+right->move_absolute(50, 127);
+right2->move_absolute(50, 127);
 }
 
 void Lift::zeroh()
@@ -40,34 +40,34 @@ right2->move_absolute(0, 127);
 
 void Lift::lowposth()
 {
-left->move_absolute(-550, 127);
-left2->move_absolute(-550, 127);
-right->move_absolute(-550, 127);
-right2->move_absolute(-550, 127);
+left->move_absolute(550, 127);
+left2->move_absolute(550, 127);
+right->move_absolute(550, 127);
+right2->move_absolute(550, 127);
 }
 
 void Lift::lowpostfliph()
 {
-left->move_absolute(-300, 127);
-left2->move_absolute(-300, 127);
-right->move_absolute(-300, 127);
-right2->move_absolute(-300, 127);
+left->move_absolute(300, 127);
+left2->move_absolute(300, 127);
+right->move_absolute(300, 127);
+right2->move_absolute(300, 127);
 }
 
 void Lift::highposth()
 {
-left->move_absolute(-875, 127);
-left2->move_absolute(-875, 127);
-right->move_absolute(-875, 127);
-right2->move_absolute(-875, 127);
+left->move_absolute(875, 127);
+left2->move_absolute(875, 127);
+right->move_absolute(875, 127);
+right2->move_absolute(875, 127);
 }
 
 void Lift::highpostfliph()
 {
-left->move_absolute(-550, 127);
-left2->move_absolute(-550, 127);
-right->move_absolute(-550, 127);
-right2->move_absolute(-550, 127);
+left->move_absolute(550, 127);
+left2->move_absolute(550, 127);
+right->move_absolute(550, 127);
+right2->move_absolute(550, 127);
 }
 
 void Lift::printMotorTemps()
